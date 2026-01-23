0c9d0cdbe9e7878d2a7d30eb6bd3c94e6b989dc8
Peixian <Peixian.Liang@Pennmedicine.upenn.edu>
2026-01-23 17:05:25 +0000

    update samples

--- Scripts/inference.sh
+++ Scripts/inference.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES=0
-CODE_DIR=/home/peixian/PASeg/
+CODE_DIR=/home/peixian/VISTA-PATH
 cd ${CODE_DIR}
 
 source activate PathSeg
 
 dataset_name=TCGA-COAD
 
-# python3 inference.py \
-#   --infer_vis_dir ./results/${dataset_name} \
-#   --checkpoint_file checkpoints/pytorch_model.bin \
-#   --image_file /data/TCGA-COAD/20x_images/TCGA-AZ-6608-01Z-00-DX1.40d9f93f-f7d8-4138-9af1-bb579c53194b.tif \
-#   --bbx_random 1 \
-#   --class_names "Tumor" "Stroma" \
-#   --overlap 1000
-
 python3 inference.py \
-  --infer_vis_dir ./results/3D \
+  --infer_vis_dir ./results/${dataset_name} \
   --checkpoint_file checkpoints/pytorch_model.bin \
-  --image_file /home/peixian/PASeg/3D/virtual_HE_z00002.tif \
+  --image_file ./examples/TCGA-COAD/TCGA-AD-6895-01Z-00-DX1.7FB1FBC6-683B-4285-89D1-A7A20F07A9D4.svs \
   --bbx_random 1 \
-  --class_names "Glands" \
-  --overlap 256
+  --class_names "Tumor" "Stroma" \
+  --overlap 512
